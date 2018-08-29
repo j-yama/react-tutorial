36733648798f443be5c9bf4a219c080e6401acdc
j-yama <j-yama@example.com>
2018-08-29 22:05:10 +0900

    Squareコンポーネントをclassからfunctionに変更した

--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => this.props.onClick()}
-            >
-                {this.props.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
@@ -34,7 +29,7 @@ class Board extends React.Component {
             <Square
                 value={this.state.squares[i]}
                 onClick={() => this.handleClick(i)}
-             />
+            />
         );
     }
 
